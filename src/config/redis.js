6e69653017ef43a7331ff26c17db8e2ba77d0e8d
BenQ <benq@benQs-MacBook-Air.local>
2023-02-20 13:56:07 +0700

    Change scripts

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,10 +3,8 @@ const config = require('./env');
 
 const redisClient =
     config.NODE_ENV === 'production' ? redis.createClient({ url: config.REDIS_URL }) : redis.createClient();
-console.log(
-    config.NODE_ENV,
-    config.REDIS_URL,
-)(async () => {
+
+(async () => {
     try {
         await redisClient.connect();
         console.log('Connection to Redis has been established successfully.');
