2b3d81b075a7bab747d1c31e9b14b5c9dad2d6b4
BenQ <benq@benQs-MacBook-Air.local>
2023-02-20 13:26:30 +0700

    Change scripts

--- src/config/config.js
+++ src/config/config.js
@@ -7,15 +7,15 @@ module.exports = {
         host: config.DB_HOST,
         dialect: config.DB_DIALECT,
         port: config.DB_PORT,
-        // logging: true,
+        logging: false,
     },
     production: {
-        username: config.DB_USER,
-        password: config.DB_PASSWORD,
-        database: config.DB_NAME,
-        host: config.DB_HOST,
-        dialect: config.DB_DIALECT,
-        port: config.DB_PORT,
-        // logging: true,
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        },
     },
 };
--- src/config/env/index.js
+++ src/config/env/index.js
@@ -13,13 +13,13 @@ const config = {
     DB_PORT: process.env.DB_PORT,
     DB_DIALECT: process.env.DB_DIALECT,
     DB_NAME: process.env.DB_NAME,
-    DB_URL: process.env.DB_URL,
+    // DB_URL: process.env.DB_URL,
 
     REDIS_USER: process.env.REDIS_USER,
     REDIS_PASSWORD: process.env.REDIS_PASSWORD,
     REDIS_HOST: process.env.REDIS_HOST,
     REDIS_PORT: process.env.REDIS_PORT,
-    REDIS_URL: process.env.REDIS_URL,
+    // REDIS_URL: process.env.REDIS_URL,
 
     CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
     CLOUDINARY_KEY: process.env.CLOUDINARY_KEY,
