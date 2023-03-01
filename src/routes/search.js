a3324245897fb7ad3d4d90f8edd09a4d9144e766
BenQ <BenghiVan@gmail.com>
2023-03-01 19:11:52 +0700

    Change Scripts

--- src/routes/search.js
+++ src/routes/search.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const searchController = require('../app/controllers//SearchController');
 const { requireAuth } = require('../middlewares/AuthMiddleware');
 
-router.get('/gigs', requireAuth, searchController.searchGigs);
-router.get('/users', requireAuth, searchController.searchUsers);
+router.get('/gigs', searchController.searchGigs);
+router.get('/users', searchController.searchUsers);
 
 module.exports = router;
